3eac04201656f5ae64016170f2df52da468a53ed
Lucho <sportelliluciano@gmail.com>
2021-02-18 17:31:11 -0300

    Agrego ejemplo de serde/csv para generar transacciones

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate rand;
+extern crate csv;
+extern crate serde;
+
 use std::{
     sync::Arc,
     sync::Mutex,
@@ -5,9 +9,64 @@ use std::{
     thread
 };
 
+use serde::{Serialize, Serializer};
+
+enum TipoTransaccion {
+    CashIn,
+    CashOut
+}
+
+impl Serialize for TipoTransaccion {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(match self {
+            TipoTransaccion::CashIn => "cash_in",
+            TipoTransaccion::CashOut => "cash_out"
+        })
+    }
+}
+
+#[derive(Serialize)]
+struct Transaccion {
+    #[serde(rename = "Transaction")]
+    id_transaccion: u32,
+    #[serde(rename = "User_id")]
+    id_cliente: u32,
+    #[serde(rename = "Timestamp")]
+    timestamp: u32,
+    #[serde(rename = "Type")]
+    tipo: TipoTransaccion,
+    #[serde(rename = "Amount")]
+    monto: f32
+}
+
+fn generar_transacciones(n_clientes: u32, n_transacciones: u32) {
+    use rand::prelude::*;
+    let mut rng = rand::thread_rng();
+    let mut file = csv::Writer::from_path("transacciones.csv").unwrap();
+
+    for id_transaccion in 1..(n_transacciones+1) {
+        let id_cliente: u32 = rng.gen_range(0..n_clientes);
+        let timestamp: u32 = rng.gen();
+        let tipo = if rng.gen() {
+            TipoTransaccion::CashIn
+        } else {
+            TipoTransaccion::CashOut
+        };
+        let monto = rng.gen::<u32>() as f32 / 1000.0;
+
+        file.serialize(Transaccion {
+            id_transaccion, id_cliente, timestamp, tipo, monto
+        }).unwrap();
+    }
+    file.flush().unwrap();
+}
 
 fn main() {
-    let (tx, rx) = channel();
+    generar_transacciones(10, 1000);
+    /*let (tx, rx) = channel();
 
     let rx_2 = Arc::new(Mutex::new(rx));
     let rx_3 = rx_2.clone();
@@ -39,5 +98,5 @@ fn main() {
     }
 
     t1.join().unwrap();
-    t2.join().unwrap();
+    t2.join().unwrap();*/
 }
