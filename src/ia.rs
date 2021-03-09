e82df7af342058c35231f056f5ba41a5314c4662
Franco Liberali <franco.liberali@gmail.com>
2021-03-08 22:05:20 -0300

    tests para procesar ia

--- src/ia.rs
+++ src/ia.rs
@@ -107,3 +107,79 @@ impl ProcesadorIA {
         self.tx_transacciones_validas.send(transaccion_validada).expect("Channel cerrado");
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::mpsc::channel;
+
+    use super::*;
+    use uuid::Uuid;
+    use crate::transaccion::{Transaccion, TransaccionAutorizada, TipoTransaccion};
+
+    #[test]
+    fn procesador_ia_enviar_transaccion_validada_sino_detecta_lavado_de_dinero() {
+        let id_transaccion = 2;
+        let transaccion = Transaccion {
+            id: id_transaccion,
+            id_cliente: Uuid::new_v4(),
+            timestamp: 112315846_128,
+            tipo: TipoTransaccion::CashIn,
+            monto: 123.33
+        };
+        let hash = Uuid::new_v4();
+        let transaccion_autorizada = TransaccionAutorizada {
+            transaccion: transaccion,
+            autorizacion: hash
+        };
+
+        let (tx_transacciones_autorizadas, rx_transacciones_autorizadas_) = channel();
+        let rx_transacciones_autorizadas = Arc::new(Mutex::new(rx_transacciones_autorizadas_));
+        let (tx_transacciones_validadas, rx_transacciones_validadas) = channel();
+
+        tx_transacciones_autorizadas.send(transaccion_autorizada).unwrap();
+
+        ProcesadorIA::iniciar(crear_logger(),
+                   rx_transacciones_autorizadas,
+                   tx_transacciones_validadas,
+                   Arc::new(Mutex::new(StdRng::seed_from_u64(2_64))));
+        let recibida = rx_transacciones_validadas.recv().unwrap();
+        assert_eq!(recibida.transaccion.id, id_transaccion);
+        assert_eq!(recibida.autorizacion, hash);
+    }
+
+    #[test]
+    fn procesador_ia_no_envia_transaccion_validada_si_detecta_lavado_de_dinero() {
+        let id_transaccion = 2;
+        let transaccion = Transaccion {
+            id: id_transaccion,
+            id_cliente: Uuid::new_v4(),
+            timestamp: 112315846_128,
+            tipo: TipoTransaccion::CashIn,
+            monto: 123.33
+        };
+        let hash = Uuid::new_v4();
+        let transaccion_autorizada = TransaccionAutorizada {
+            transaccion: transaccion,
+            autorizacion: hash
+        };
+
+        let (tx_transacciones_autorizadas, rx_transacciones_autorizadas_) = channel();
+        let rx_transacciones_autorizadas = Arc::new(Mutex::new(rx_transacciones_autorizadas_));
+        let (tx_transacciones_validadas, rx_transacciones_validadas) = channel();
+
+        tx_transacciones_autorizadas.send(transaccion_autorizada).unwrap();
+
+        let handle = ProcesadorIA::iniciar(crear_logger(),
+                   rx_transacciones_autorizadas,
+                   tx_transacciones_validadas,
+                   Arc::new(Mutex::new(StdRng::seed_from_u64(34_64))));
+        drop(tx_transacciones_autorizadas);
+        handle.join().unwrap();
+        let resultado = rx_transacciones_validadas.try_recv();
+        assert!(resultado.is_err());
+    }
+
+    fn crear_logger() -> TaggedLogger {
+        TaggedLogger::new("PROCESADOR IA", Arc::new(Logger::new_to_stdout()))
+    }
+}
